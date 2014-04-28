5587918b4ba8eebc97e290424b7655477eae44e9
angelafolz <angelafolz@gmail.com>
2014-04-28 12:01:56 -0500

    refactor to receive highest number input

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
 	var body = $("body");
+	var input = $("input");
+	var playBtn = $("button");
 
-	$("button").click(function() {
-		for (var i = 1; i <= 100; i++) {
+	// count and determine fizz/buzz
+	var playFizzBuzz = function(num) {
+		body.append("<br><br>");
+		for (var i = 1; i <= num; i++) {
 			if (i % 3 === 0 && i % 5 === 0) {
 				body.append("fizzbuzz, ");
 			} else if (i % 3 === 0) {
@@ -14,21 +18,25 @@ $(document).ready(function(){
 				body.append(i + ", ");
 			}
 		}
-	
-		// alternate method
-		/*for (var i = 1; i <= 100; i++) {
-			if (i % 3 === 0) {
-				body.append("fizz");
-				if (i % 5 === 0) {
-					body.append("buzz, ");
-				} else {
-					body.append(", ");
-				}
-			} else if (i % 5 === 0) {
-				body.append("buzz, ");
-			} else {
-				body.append(i + ", ");
-			}
-		}*/
+	};
+
+	// get input for highest number
+	playBtn.click(function() {
+		var value = input.val();
+		if (value < 1000) {
+			playFizzBuzz(value);
+		} else if (value >= 1000) {
+			alert("I may be a computer, but I don't have all day. Try a number less than 1000.");
+		} else {
+			alert("That's not an integer, silly.");
+		}
+		input.val("").focus();
+	});
+
+	// pressing enter same as clicking button
+	input.keypress(function(event){
+		if (event.which == 13){
+			playBtn.click();
+		}
 	});
 });
