15306803af1841c48f051fc45e8a7f00cd40d0de
angelafolz <angelafolz@gmail.com>
2014-04-27 22:54:38 -0500

    basic fizz buzz functionality

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	var body = $("body");
+
+	for (var i = 1; i <= 100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			body.append("fizzbuzz" + ", ");
+		} else if (i % 3 === 0) {
+			body.append("fizz" + ", ");
+		} else if (i % 5 === 0) {
+			body.append("buzz" + ", ");
+		} else {
+			body.append(i + ", ");
+		}
+	}
+
+});
