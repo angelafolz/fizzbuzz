7e6ef18adbd15a8e05a806287b1d62bce74ec114
angelafolz <angelafolz@gmail.com>
2014-04-27 23:42:34 -0500

    play button, alternate method in comment

--- js/app.js
+++ js/app.js
@@ -2,16 +2,33 @@ $(document).ready(function(){
 
 	var body = $("body");
 
-	for (var i = 1; i <= 100; i++) {
-		if (i % 3 === 0 && i % 5 === 0) {
-			body.append("fizzbuzz" + ", ");
-		} else if (i % 3 === 0) {
-			body.append("fizz" + ", ");
-		} else if (i % 5 === 0) {
-			body.append("buzz" + ", ");
-		} else {
-			body.append(i + ", ");
+	$("button").click(function() {
+		for (var i = 1; i <= 100; i++) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				body.append("fizzbuzz, ");
+			} else if (i % 3 === 0) {
+				body.append("fizz, ");
+			} else if (i % 5 === 0) {
+				body.append("buzz, ");
+			} else {
+				body.append(i + ", ");
+			}
 		}
-	}
-
+	
+		// alternate method
+		/*for (var i = 1; i <= 100; i++) {
+			if (i % 3 === 0) {
+				body.append("fizz");
+				if (i % 5 === 0) {
+					body.append("buzz, ");
+				} else {
+					body.append(", ");
+				}
+			} else if (i % 5 === 0) {
+				body.append("buzz, ");
+			} else {
+				body.append(i + ", ");
+			}
+		}*/
+	});
 });
